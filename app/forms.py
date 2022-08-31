5c735f942e5fe0a77dd9574c199d8b3ee045c248
despinoza1989 <73562537+despinoza1989@users.noreply.github.com>
2022-08-31 19:35:41 -0400

    Se agrega forms Administrativo, Cliente y Profesional

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,31 @@
 from socket import fromshare
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Administrativo, Profesional, Cliente
 
 class CustomUserCreationForm(UserCreationForm):
     
     class Meta:
         model = User
         fields = ['username', "first_name", "last_name", "email","password1", "password2"]
+
+class AdministrativoForm(forms.ModelForm):
+
+    class Meta: 
+        model = Administrativo
+
+        fields = '__all__'
+
+class ClienteForm(forms.ModelForm):
+
+    class Meta: 
+        model = Cliente
+
+        fields = '__all__'
+
+class ProfesionalForm(forms.ModelForm):
+
+    class Meta: 
+        model = Profesional
+
+        fields = '__all__'
